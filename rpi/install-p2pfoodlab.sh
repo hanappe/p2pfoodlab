abb287a94e278f2f644787687b168eb6e22835fa
hanappe <hanappe@csl.sony.fr>
2013-12-16 10:55:45 +0100

    using git for code download

--- rpi/install-p2pfoodlab.sh
+++ rpi/install-p2pfoodlab.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+curdir=`pwd`
+#uid=`id -u`
+#gid=`id -g`
+uid=pi
+gid=pi
+home=/home/pi
+dest=/var/p2pfoodlab
+
 # Install required packages. Also remove package ifplugd because it is
 # doing a bad job on the Raspberry Pi in combination with the DHCP
 # server isc-dhcp-server.
 apt-get update
-apt-get install apache2 php5 libapache2-mod-php5 gcc libjpeg8-dev i2c-tools libi2c-dev isc-dhcp-server
+apt-get install apache2 php5 libapache2-mod-php5 gcc libjpeg8-dev i2c-tools libi2c-dev isc-dhcp-server git
 apt-get purge ifplugd
 apt-get autoremove
 
@@ -13,47 +21,49 @@ adduser pi i2c
 adduser pi video
 adduser pi www-data
 
-install --directory --owner=pi --group=pi --mode=0700 /home/pi/.ssh
-install --directory --owner=pi --group=www-data --mode=0775 /etc/opensensordata
+install --directory --owner=$uid --group=$gid --mode=0700 $home/.ssh
+install --directory --owner=$uid --group=www-data --mode=0775 /etc/opensensordata
 
 # Load the modules for the I2C bus.
 modprobe i2c_dev
 modprobe i2c_bcm2708
 
-# Delete any trailing files from a previous download.
-rm -rf /tmp/sensorbox.tar.gz /tmp/dist
-
 # Download the latest version of the sensorbox software and
 # configuration files.
-curl --output /tmp/sensorbox.tar.gz https://p2pfoodlab.net/sensorbox-versions/sensorbox-latest.tar.gz
-cd /tmp
-tar xzf sensorbox.tar.gz
-chown -R pi.pi dist
+cd $home
+if [ -d p2pfoodlab ]; then
+    cd p2pfoodlab
+    git pull 
+else
+    git clone https://github.com/hanappe/p2pfoodlab.git
+    cd p2pfoodlab
+fi
 
 # Compile the source code.
-su -l -c "make -C /tmp/dist/src clean all" pi
+su -l -c "make -C rpi/src clean all" pi
 
 # Install the files.
-cd /tmp/dist
-bash ./install.sh /var/p2pfoodlab pi pi
+cd dist
+bash ./install.sh $dest $uid $gid
+cd ..
 
 # Install the P2P Food Lab configuration file.
-install --directory --owner=pi --group=www-data --mode=0775 /var/p2pfoodlab
-install --directory --owner=pi --group=www-data --mode=0775 /var/p2pfoodlab/backup
-install --directory --owner=pi --group=www-data --mode=0775 /var/p2pfoodlab/photostream
-install --directory --owner=pi --group=www-data --mode=0775 /var/p2pfoodlab/etc
-install --owner=pi --group=www-data --mode=0664 config.json /var/p2pfoodlab/etc
+install --directory --owner=$uid --group=www-data --mode=0775 $dest
+install --directory --owner=$uid --group=www-data --mode=0775 $dest/backup
+install --directory --owner=$uid --group=www-data --mode=0775 $dest/photostream
+install --directory --owner=$uid --group=www-data --mode=0775 $dest/etc
+install --owner=$uid --group=www-data --mode=0664 config.json $dest/etc
 
-touch /var/p2pfoodlab/log.txt
-chown pi.www-data /var/p2pfoodlab/log.txt
-chmod 0664 /var/p2pfoodlab/log.txt
-
+touch $dest/log.txt
+chown pi.www-data $dest/log.txt
+chmod 0664 $dest/log.txt
 
 # Install the system configuration files.
 source ./files.txt
 for file in ${etc[@]}; do
     install --owner=root --group=root $file /$file
 done
+
 
 # Install the start-up scripts.
 update-rc.d p2pfoodlab start 99 2 3 4 5 . stop 99 0 6 .
@@ -66,4 +76,6 @@ service apache2 restart
 
 # Enable the DHCP server on eth0
 update-rc.d isc-dhcp-server enable
+
+cd $curdir
 
