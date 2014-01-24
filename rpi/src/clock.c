b9419ebe92bf176731e6b7c042c4580b27158dc1
hanappe <hanappe@csl.sony.fr>
2014-01-24 16:52:08 +0100

    new updater app

--- rpi/src/clock.c
+++ rpi/src/clock.c
@@ -115,7 +115,7 @@ int clock_set(sensorbox_t* box)
         log_info("Clock: Setting system time.");
 
         if (network_connected() == 1) {
-                log_info("Clock: Connected to network. Let ntpd do its job.");
+                log_info("Clock: Connected to network. Let ntpd do the job.");
                 return 0;
         }
 
