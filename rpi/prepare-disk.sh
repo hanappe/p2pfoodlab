de38a01aaf5b6eb9dbb19801f5a9e6ca0e0c881c
hanappe <hanappe@csl.sony.fr>
2014-06-13 20:58:26 +0200

    generate sys files

--- rpi/prepare-disk.sh
+++ rpi/prepare-disk.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm /var/p2pfoodlab/etc/config.json
 rm /var/p2pfoodlab/etc/opensensordata/*
 rm -rf ~/.ssh/
@@ -12,5 +14,9 @@ make
 
 cp /home/pi/p2pfoodlab/rpi/bin/sensorbox /var/p2pfoodlab/bin/sensorbox 
 cp /home/pi/p2pfoodlab/rpi/config.json /var/p2pfoodlab/etc/config.json
+
+sudo /var/p2pfoodlab/bin/sensorbox -l - -D generate-system-files
+sudo rm -rf /var/p2pfoodlab/backup/*
+
 sudo cp /home/pi/p2pfoodlab/rpi/boot/p2pfoodlab.json /boot/p2pfoodlab.json
 
