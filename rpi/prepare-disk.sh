79bf4179c8ef7b81dfb98ab684531f389f3504b9
hanappe <hanappe@csl.sony.fr>
2014-06-13 12:55:07 +0200

    memo for preparing disk images

---
+++ rpi/prepare-disk.sh
@@ -0,0 +1,16 @@
+rm /var/p2pfoodlab/etc/config.json
+rm /var/p2pfoodlab/etc/opensensordata/*
+rm -rf ~/.ssh/
+sudo rm -f /boot/p2pfoodlab*.json*
+> /var/p2pfoodlab/log.txt
+rm /home/pi/.bash_history 
+sudo rm /root/.bash_history 
+
+cd /home/pi/p2pfoodlab/rpi/src/
+git pull
+make
+
+cp /home/pi/p2pfoodlab/rpi/bin/sensorbox /var/p2pfoodlab/bin/sensorbox 
+cp /home/pi/p2pfoodlab/rpi/config.json /var/p2pfoodlab/etc/config.json
+sudo cp /home/pi/p2pfoodlab/rpi/boot/p2pfoodlab.json /boot/p2pfoodlab.json
+
