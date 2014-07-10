2f509b866d82927c4dcbde96199a8390d9a0f2c1
hanappe <hanappe@csl.sony.fr>
2014-07-10 13:53:04 +0200

    testing dashboard code

---
+++ https/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="http://code.createjs.com/easeljs-0.7.1.min.js"></script>
+    <script>
+        function init() {
+                 var circle = new createjs.Shape();
+                 circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+                 circle.x = 100;
+                 circle.y = 100;
+                 stage.addChild(circle);
+        }
+    </script>
+</head>
+<body onLoad="init();">
+    <canvas id="demoCanvas" width="500" height="300">
+        alternate content
+    </canvas>
+</body>
+</html>
+
